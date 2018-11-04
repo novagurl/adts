15bde87931771b486a8c4752534c5f55064d0703
novagurl <novagurl@users.noreply.github.com>
2018-11-04 16:59:18 -0400

    List.cpp

--- List.cpp
+++ List.cpp
@@ -108,11 +108,8 @@ void List::clear()
 
 int List::get(int k)
 	{
-	
-	if (k < 1 or k > num_elements)//if the location is invalid 
-	     throw out_of_range("List::remove(" +to_string(k)+") failed. (valid indices are 1 to "+to_string(num_elements)+")");//throw an 	"out_of_range" exception
 
-	Node * tmpPtr = frontPtr;
+	Node* tmpPtr = frontPtr;
 	for (int i=1; i <=k-1; i++)
 	{
 	tmpPtr=tmpPtr -> link;
